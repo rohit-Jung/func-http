faf053c88b9b7347c6c47c2931e15aadbbceb199
rohitJung <rokshh28@gmail.com>
2026-05-22 23:46:43 +0545

    feat: parsing request line done

--- internal/request/request.go
+++ internal/request/request.go
@@ -2,7 +2,9 @@
 package request
 
 import (
+	"bytes"
 	"errors"
+	"fmt"
 	"io"
 	"strings"
 )
@@ -10,8 +12,9 @@ import (
 type State string
 
 const (
-	Init State = "init"
-	Done State = "done"
+	StateInit  State = "init"
+	StateDone  State = "done"
+	StateError State = "error"
 )
 
 type RequestLine struct {
@@ -30,67 +33,117 @@ type Request struct {
 const CRLF = "\r\n"
 
 var (
-	malformedRequest       = "[MALFORMED] error while reading request"
-	malformedRequestLine   = "[MALFORMED] error while reading request line"
-	malformedHTTPVersion   = "[MALFORMED] couldn't determine the httpversion"
-	malformedHTTPMethod    = "[MALFORMED] http method is malformed"
-	unsupportedHTTPVersion = "[UNSUPPORTED] http version currently not supported"
+	errMalformedRequestLine   = fmt.Errorf("[MALFORMED] error while reading request line")
+	errMalformedHTTPVersion   = fmt.Errorf("[MALFORMED] couldn't determine the httpversion")
+	errMalformedHTTPMethod    = fmt.Errorf("[MALFORMED] http method is malformed")
+	errRequestState           = fmt.Errorf("[STATE ERROR] error in request state")
+	errUnsupportedHTTPVersion = fmt.Errorf("[UNSUPPORTED] http version currently not supported")
 )
 
-// func (r *Request) parse(data []byte) (int, error) {}
-
-func parseRequestLine(buf []byte) (*RequestLine, error) {
-	body := strings.Split(string(buf), CRLF)
-	if len(body) < 2 {
-		return nil, errors.New(malformedRequest)
+func parseRequestLine(buf []byte) (*RequestLine, int, error) {
+	indexOfCrlf := bytes.Index(buf, []byte(CRLF))
+	if indexOfCrlf == -1 {
+		return nil, 0, nil
 	}
 
-	startLine := body[0]
+	startLine := buf[:indexOfCrlf]
+	readBytes := indexOfCrlf + len(CRLF)
 
 	// GET / HTTP/1.1
-	parts := strings.Split(startLine, " ")
+	parts := bytes.Split(startLine, []byte(" "))
 	if len(parts) != 3 {
-		return nil, errors.New(malformedRequestLine)
+		return nil, 0, errMalformedRequestLine
 	}
 
-	method := parts[0]
-	if strings.ToUpper(method) != method {
-		return nil, errors.New(malformedHTTPMethod)
+	httpMethod := parts[0]
+	if strings.ToUpper(string(httpMethod)) != string(httpMethod) {
+		return nil, 0, errMalformedHTTPMethod
 	}
 
-	httpVersion := strings.Split(parts[2], "/")
+	httpVersion := bytes.Split(parts[2], []byte("/"))
 	if len(httpVersion) != 2 {
-		return nil, errors.New(malformedHTTPVersion)
+		return nil, 0, errMalformedHTTPVersion
 	}
 
-	if httpVersion[1] != "1.1" {
-		return nil, errors.New(unsupportedHTTPVersion)
+	if !bytes.Equal(httpVersion[1], []byte("1.1")) {
+		return nil, 0, errUnsupportedHTTPVersion
 	}
 
 	reqLine := &RequestLine{
-		Method:        parts[0],
-		RequestTarget: parts[1],
-		HTTPVersion:   httpVersion[1],
+		Method:        string(parts[0]),
+		RequestTarget: string(parts[1]),
+		HTTPVersion:   string(httpVersion[1]),
 	}
 
-	return reqLine, nil
+	return reqLine, readBytes, nil
+}
+
+func newRequest() *Request {
+	return &Request{
+		State: StateInit,
+	}
+}
+
+func (r *Request) parse(buf []byte) (int, error) {
+	readBytes := 0
+dance:
+	for {
+		switch r.State {
+		case StateError:
+			return 0, errRequestState
+
+		case StateInit:
+			rl, n, err := parseRequestLine(buf[readBytes:])
+			if err != nil {
+				r.State = StateError
+				return 0, err
+			}
+
+			if n == 0 {
+				break dance
+			}
+
+			r.RequestLine = rl
+			readBytes += n
+			r.State = StateDone
+
+		case StateDone:
+			break dance
+		}
+	}
+	return readBytes, nil
+}
+
+func (r *Request) done() bool {
+	return r.State == StateDone || r.State == StateError
 }
 
 func RequestFromReader(reader io.Reader) (*Request, error) {
-	buf, err := io.ReadAll(reader)
-	if err != nil {
-		return nil, errors.New(malformedRequestLine)
-	}
+	req := newRequest()
 
-	reqLine, err := parseRequestLine(buf)
-	if err != nil {
-		return nil, err
-	}
+	buf := make([]byte, 1024)
+	bufLen := 0
 
-	req := &Request{
-		RequestLine: reqLine,
-		Headers:     map[string]string{},
-		Body:        "",
+	for !req.done() {
+		bytesRead, err := reader.Read(buf[bufLen:])
+		if err != nil {
+			if errors.Is(err, io.EOF) {
+				break
+			}
+
+			return nil, err
+		}
+
+		// parsed till read ones
+		bufLen += bytesRead
+		bytesParsed, err := req.parse(buf[:bufLen])
+		if err != nil {
+			return nil, err
+		}
+
+		// copy back whats not parsed and start from where parsing was stopped
+		copy(buf, buf[bytesParsed:bufLen])
+		bufLen -= bytesParsed
 	}
 
 	return req, nil
